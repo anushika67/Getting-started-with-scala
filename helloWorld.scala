8481edc7eef53d156d0bdd2659ca840b2f3a2eae
anushika67 <anushikaanand67@gmail.com>
2018-07-04 12:29:20 +0530

    Pertaining to the Tradition Helloworld printed

---
+++ helloWorld.scala
@@ -0,0 +1,9 @@
+object helloWorld{
+	
+	def main(args:Array[String]):Unit={					    																
+		println("Hello,World")						 
+
+	}
+}
+
+ /**unit is same as void in java this function returns nothing **/
