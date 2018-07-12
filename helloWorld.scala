ecab1a6180d9bc0ccbf315ff35f5a9276e7b5c90
anushika67 <anushikaanand67@gmail.com>
2018-07-12 11:03:50 +0530

    cliche examples added

--- helloWorld.scala
+++ helloWorld.scala
@@ -1,9 +1,10 @@
+/** Being an Object Oriented Language everything is an object **/
 object helloWorld{
 	
 	def main(args:Array[String]):Unit={					    																
 		println("Hello,World")						 
 
 	}
-}
 
- /**unit is same as void in java this function returns nothing **/
+}
+ /**unit is same as void in java , 	this function returns nothing **/
